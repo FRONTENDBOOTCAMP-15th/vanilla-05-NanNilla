115dd9d65abe644fbfe9e001656d26365535110c
nomineaquae <yoojin629@gmail.com>
2025-11-14 16:16:08 +0900

    fix : 사이드 바 버튼 이동 경로 수정

--- src/components/header.ts
+++ src/components/header.ts
@@ -21,8 +21,8 @@ class HeaderComponent extends HTMLElement {
       <div class="flex flex-col gap-9.5">
         <input type="checkbox" id="menu" class="peer hidden"><label for="menu" class="w-9 h-9 absolute right-2 p-1 z-30 box-border bg-[url(/assets/icon36px/icon-close.svg)] cursor-pointer"></label>
       <div class="flex gap-5 pl-8.5 pt-19">
-        <button class="w-22.5 h-10 rounded-full bg-nike-black text-nike-white text-base"><a href="../../src/pages/sign-up.html">가입하기</a></button>
-        <button class="w-19.5 h-10 rounded-full bg-nike-white text-nike-black border border-nike-gray-light text-base"><a href="../../src/pages/sign-in.html">로그인</a></button>
+        <button class="w-22.5 h-10 rounded-full bg-nike-black text-nike-white text-base"><a href="../../src/pages/signup.html">가입하기</a></button>
+        <button class="w-19.5 h-10 rounded-full bg-nike-white text-nike-black border border-nike-gray-light text-base"><a href="../../src/pages/signin.html">로그인</a></button>
       </div>
       <div class="flex flex-col pl-8.5 gap-6">
         <button class="flex items-center justify-between text-2xl leading-7 font-medium"><span>New & Featured</span><img src="/assets/icon36px/icon-next.svg"></button>
