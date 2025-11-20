c4f95599fce1c477e165a4bf3a71cecf1566499a
tjwngml <tjwngml0306@gmail.com>
2025-11-20 23:52:18 +0900

    주석 추가

--- src/ts/itemdetail.ts
+++ src/ts/itemdetail.ts
@@ -4,20 +4,30 @@ import type { ItemListRes } from '../types/response';
 import type { CartItem } from './cart';
 
 const params = new URLSearchParams(window.location.search);
+// 현재 웹 페이지 URL에서 쿼리문자열을 가져와 params에 저장
 const newQuery = params.get('extra.isNew');
+// params 객체에서 extra.isNew 키에 해당하는 값을 가져와 newQuery에 저장
 const IdQuery: string | null = params.get('_id');
+// params 객체에서 id라는 키에 해당하는 값을 가져와서 IDQuery에 저장
 
 async function getData() {
+  // 비동기 함수로 getData 선언
   const axios = getAxios();
+  // getAxios 함수를 import 하여 axios라 선언
   try {
     let url = '/products';
+    // url 변수를 '/products' 로 초기화
     if (IdQuery) {
-      url = `/products?extra.category.0=${encodeURIComponent(IdQuery)}`;
+      // IDQuery에 값이 있는 경우
+      url = `/products?extra.category.0=${encodeURIComponent(IdQuery)}`; // url이 해당 값으로 변경
     } else if (newQuery) {
-      url = `/products?extra.isNew=${encodeURIComponent(newQuery)}`;
+      // IDQuery 가 없지만 newQuery에 값이 있는 경우
+      url = `/products?extra.isNew=${encodeURIComponent(newQuery)}`; // url 이 해당 값으로 변경
     }
     const { data } = await axios.get<ItemListRes>(url);
+    // axios를 사용해 위에서 결정된 url로 GET요청 보냄 . await 있으므로 서버 응답 올 때까지 기다림
     return data;
+    // 서버로부터 받은 ItemListRes를 함수를 호출한 곳으로 반환
   } catch (err) {
     console.log(err);
   }
@@ -25,7 +35,7 @@ async function getData() {
 
 const itemWrapper = document.querySelector('.item-entire-wrapper'); // item-entire-wrapper 찾기
 const itemList = document.querySelector('.item-list-wrapper') as HTMLElement; // item-list-wrapper 찾기
-const productInfo = document.querySelector('.productInfo'); // productInfo  찾기
+const productInfo = document.querySelector('.productInfo') as HTMLElement; // productInfo  찾기
 const figureTag = document.createElement('figure'); // figure 태그 생성
 
 let selectedProduct = {}; // selectedProduct를 빈 객체로 선언
@@ -43,6 +53,7 @@ productName?.appendChild(h1Tag); // <div class="productName"><main><h1></h1></ma
 productName?.appendChild(pTag); //<div class="productName"><main><h1><p></p></h1></main></div>
 productName?.appendChild(div1Tag); //<div class="productName"><main><h1><p><div></div></p></h1></main></div>
 
+///-------------------------반응형을 위한 div 묶기----------------------------/////
 if (window.innerWidth <= 960) {
   itemWrapper?.appendChild(productName); // 브레이크 포인트보다 뷰포트가 작을때 <div><div class="productName"><main><h1><p><div></div></p></h1></main></div></div>
 } else {
@@ -53,6 +64,7 @@ if (window.innerWidth >= 960) {
   itemWrapper?.appendChild(figureTag); // 브레이크 포인트보다 뷰포트가 작을때 <div ><figure></figure></div>
 }
 itemWrapper?.appendChild(itemList); // 브레이크 포인트보다 뷰포트가 클 때 <div class="item-list-wrapper "><figure></figure></div>
+///-------------------------반응형을 위한 div 묶기----------------------------/////
 
 // 상품 이름, 가격, 이미지 출력
 function render(prds: Products[]) {
@@ -62,11 +74,13 @@ function render(prds: Products[]) {
   prds?.map((prd) => {
     figureTag.classList.add('min-w-[360px]', 'detail-item-image', 'overflow-x-auto', 'pt-6', 'justify-center', 'items-center');
 
-    const imgTag = document.createElement('img'); // img 태그 생성
+    ///-------------------------메인 이미지----------------------------/////
+
+    const imgTag = document.createElement('img'); // img 태그 생성 ( 메인 이미지)
     if (prd.mainImages && prd.mainImages.length > 0) {
       // 만일 데이터에 mainImages속성이 존재하고 유효한 이미지 데이터 또한 존재한다면
       imgTag.src = prd.mainImages[0].path;
-      // 생성되는 이미지 태그의 src는 그 배열의 첫번째 인덱스의 path로 넣는다.
+      // 생성되는 이미지 태그의 src는 그 배열의 첫번째 인덱스의 path로 한다. (메인 이미지)
     }
     imgTag.alt = prd.name + '이미지';
     // 대체 이미지는 prd.name + 이미지로 한다
@@ -76,13 +90,17 @@ function render(prds: Products[]) {
     // div 태그 생성 하고 colorList라고 한다
     colorList.classList.add('min-h-[150px]', 'flex', 'overflow-x-auto');
     // 스타일 추가
+    ///-------------------------메인 이미지----------------------------/////
+
+    ///-------------------------상세 이미지 및 다른 색상 이미지 출력----------------------------/////
 
     // 색상 버튼 생성 루프
     prd.mainImages.map((image, index) => {
       const itemColorButton = document.createElement('button') as HTMLButtonElement;
       // button 태그 생성
       const itemImage = document.createElement('img') as HTMLImageElement;
-      // img 태그 생성
+      // img 태그 생성 - 아래의 작은 상세 이미지들
+      ///-------------------------상세 이미지 및 다른 색상 이미지 출력----------------------------/////
 
       // 버튼 스타일 설정
       itemColorButton.classList.add(
@@ -101,6 +119,8 @@ function render(prds: Products[]) {
       itemImage.classList.add('w-full', 'h-full', 'object-cover', 'block');
       itemImage.src = image.path;
       itemImage.alt = `${prd.name} - ${image.name}`;
+
+      ///-------------------------색상 선택 시 테두리 나타나는 기능 ----------------------------/////
 
       // [초기 상태 설정]
       // 첫 번째는 검은 테두리, 나머지는 투명 테두리
@@ -130,18 +150,26 @@ function render(prds: Products[]) {
         itemColorButton.classList.add('border-black');
 
         console.log('선택된 상품:', selectedProduct);
+        ///-------------------------색상 선택 시 테두리 나타나는 기능 ----------------------------/////
       });
 
       itemColorButton.appendChild(itemImage);
+      // 버튼 태그 안에 상세이미지 태그 넣기 ( 누르면 선택 할 수 있도록)
+
       colorList?.appendChild(itemColorButton);
+      // 그 버튼를 colorList div로 묶기
     });
     productInfo?.appendChild(colorList);
+    // productInfo 안에 colorList div 집어넣기
 
     mainTag.classList.add('detail-main', 'pt-20', 'flow-root');
+    // mainTag 스타일 추가
 
     h1Tag.classList.add('h-8.75', 'ml-6', 'font-medium', 'text-xl', 'font-Noto');
     h1Tag.textContent = prd.name;
+    // <h1>prd.name</h1>
 
+    // 성별 카테고리에 따라 pTag 렌더링 다르게 하기
     pTag.classList.add('h-7', 'ml-6', 'font-medium', 'font-Noto');
     if (prds[0].extra.gender === 'men') {
       pTag.textContent = '남성 신발';
@@ -150,43 +178,60 @@ function render(prds: Products[]) {
     } else if (prd.extra.gender === 'kids') {
       pTag.textContent = '키즈 신발';
     }
+
     pTag.textContent = prd.extra.gender;
+    //<p>prd.extar.gender</p>
 
     div1Tag.classList.add('detail-price-info', 'pt-3', 'ml-6', 'flex');
-
+    // div1Tag에 detail-price-info 클래스 추가 및 디자인 추가
     const span1Tag = document.createElement('span');
+    // span 태그 생성
     span1Tag.classList.add('inline-block', 'h-7', 'mr-2', 'font-medium', 'font-Noto');
     span1Tag.textContent = String(prd.price.toLocaleString());
+    // 디자인 추가하고 <span>prd.price.toLocaleString()</span>
 
     const sTag = document.createElement('s');
+    // s 태그 생성
     sTag.classList.add('inline-block', 'h-7', 'mr-2', 'font-medium', 'font-Noto', 'text-nike-gray-medium');
     sTag.textContent = String(prd.price.toLocaleString());
+    // <s>prd.price.toLocaleString()</s>
 
     const span2Tag = document.createElement('span');
+    // span 태그 생성
     span2Tag.classList.add('inline-block', 'h-7', 'font-medium', 'font-Noto', 'text-nike-green');
     span2Tag.textContent = String(prd.price.toLocaleString());
+    // <span>prd.price.toLocaleString()</span>
 
     div1Tag.appendChild(span1Tag);
+    // <div class="productName"><main><h1><p><div><span></span></div></p></h1></main></div>
     div1Tag.appendChild(sTag);
+    // <div class="productName"><main><h1><p><div><span><s></s></span></div></p></h1></main></div>
     div1Tag.appendChild(span2Tag);
-
+    // <div class="productName"><main><h1><p><div><span><s><span></span></s></span></div></p></h1></main></div>
     if (window.innerWidth <= 960) {
       itemList?.appendChild(figureTag); // 위에서 미리 만든 figureTag 추가
     }
-
+    //<div class="item-list-wrapper"><figure><img src="prd.mainImages[0].path;" alt ="prd.name + 이미지"></figure></div>
     itemList?.appendChild(div2Tag);
+    //<div class="item-list-wrapper"><figure><img src="prd.mainImages[0].path;" alt ="prd.name + 이미지"><div><div></figure></div>
   });
 }
 
 const data = await getData();
+// getData를 호출하고 응답이 올 때까지 기다린다
 let filteredData: Products[] = [];
+// filteredData를 선언하고 빈 배열로 초기화한다
 if (data?.ok) {
-  filteredData = data?.item;
+  // 유효성 검사 : 응답 객체 data가 존재하고 데이터가 성공적으로 로드 될 경우 내부 로직 실행
+  filteredData = data?.item; // data.item을 filteredData에 할당, IDQuery 가 없다면 data.item 전체가 화면에 렌더링될 데이터
   if (IdQuery) {
+    // 변수에 값이 존재할 때만 추가 필터링 실행
     filteredData = data.item.filter((item: Products) => String(item._id) === IdQuery);
-  }
+  } // 결과적으로 URL 파라미터로 지정된 특정 ID와 일치하는 상품만 남게됨
   render(filteredData);
 }
+///-------------------------제품 사이즈 출력 ----------------------------/////
+
 // 제품 사이즈 출력
 const axiosInstace = getAxios();
 const container = document.querySelector('.container');
@@ -228,7 +273,8 @@ async function getSizeProduct() {
       container?.append(productSize);
     }); //
   } catch (err) {
-    // 사이즈가 DB에 없을 때
+    // 타입에러 발생 - catch 문으로 빠져나가서 하드코딩 렌더링 되게
+    // 사이즈가 DB에 없을 때 - 하드코딩
     function noArray() {
       return `
 <button class="w-[56.4px] px-4 py-2 border border-gray-300 rounded-md text-sm hover:bg-gray-100" type="button ">250</button>
@@ -255,6 +301,10 @@ async function getSizeProduct() {
   }
 }
 getSizeProduct();
+
+///-------------------------제품 사이즈 출력 ----------------------------/////
+
+///-------------------------제품 사이즈 선택 - 장바구니 연동 ----------------------------/////
 
 // 사이즈 선택
 container?.addEventListener('click', (event) => {
@@ -336,4 +386,4 @@ addCartBtn?.addEventListener('click', async () => {
 });
 
 getData();
-// 로그인 할 때 로컬스토리지를의 데이터를 DB로 병합하고 로컬스토리지 삭제
+///-------------------------제품 사이즈 선택 - 장바구니 연동 ----------------------------/////
